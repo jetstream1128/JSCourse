6b7eca41a0566cd55a9a97e256d93a76fd0dc693
jetstream1128 <bogdanalexeev2007@live.com>
2023-02-19 15:58:23 +0800

    Lesson 105

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -30,8 +30,29 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Dnipro city',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({ address: 'Kiev', starterIndex: 1 });
+
+//Object Destructing
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
@@ -41,6 +62,24 @@ const {
   categories: tags,
 } = restaurant;
 console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
 
 /* ----------------------------------------
 //Destructing arrays
