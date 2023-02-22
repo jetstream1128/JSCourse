e4047427355d9528a12d7a87f0db9116fe8e3d23
jetstream1128 <bogdanalexeev2007@live.com>
2023-02-22 20:39:03 +0800

    Challenge 1

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -54,6 +54,41 @@ const restaurant = {
 
 //#region  ------------- Lection 107 ------------
 
+const rest1 = {
+  name: 'Capi',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La pizza',
+  owner: 'Giovani Rossi',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// console.log(rest1);
+// rest2.numGuests = rest2.numGuests || 10;
+// console.log(rest2);
+
+// rest1.numGuests ||= 10;
+// console.log(rest1);
+
+// rest2.numGuests ||= 10;
+// console.log(rest2);
+
+// rest1.numGuests ??= 10;
+// console.log(rest1);
+
+// rest2.owner = rest2.owner && '<Anonymous>';
+// console.log(rest2);
+// rest1.owner = rest1.owner && '<Anonymous>';
+// console.log(rest1);
+
+rest1.owner &&= '<Anonymous>';
+console.log(rest1);
+rest2.owner &&= '<Anonymous>';
+console.log(rest2);
+
 //#endregion
 
 //#region  ------------- Lection 107 ------------
