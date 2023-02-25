b941cd83ced6071caad03fb42a5f50e52701a1c7
jetstream1128 <bogdanalexeev2007@live.com>
2023-02-25 16:01:01 +0800

    Challenge 2

--- Data-Structures-Operators/challenge.js
+++ Data-Structures-Operators/challenge.js
@@ -58,24 +58,135 @@ const game = {
     team2: 6.5,
   },
 };
+//#region Challenge 1 solution
 
-const [players1, players2] = game.players;
+// const [players1, players2] = game.players;
 
-const [gk, ...fieldPlayers] = players1;
+// const [gk, ...fieldPlayers] = players1;
 
-const allPlayers = [...players1, ...players2];
+// const allPlayers = [...players1, ...players2];
 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
-const { team1, team2, x: draw } = game.odds;
-console.log(team1, team2, draw);
+// const { team1, team2, x: draw } = game.odds;
+// console.log(team1, team2, draw);
 
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// };
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 
-printGoals(...game.scored);
+// printGoals(...game.scored);
 
-team1 < team2 && console.log('Team 1 is more likely to win!');
-team1 > team2 && console.log('Team 2 is more likely to win!');
+// team1 < team2 && console.log('Team 1 is more likely to win!');
+// team1 > team2 && console.log('Team 2 is more likely to win!');
+//#endregion
+///////////////////////////////////////
+// Coding Challenge #2
+
+//Let's continue with our football betting app!
+
+//1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+//Solution:
+
+for (const [goal, name] of game.scored.entries()) {
+  console.log(`Goal ${goal + 1}: ${name}`);
+}
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+//Solution:
+const odds = Object.values(game.odds);
+let a = 0;
+for (const odd of odds) a += odd;
+a /= odds.length;
+console.log(a);
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+const oddTeams = Object.entries(game.odds);
+
+for (const [team, odd] of oddTeams) {
+  const str = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${str}: ${odd}`);
+}
+
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+let scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
+
+//for (let i = 0; i < nameScorer.length; i++) console.log(scorers);
+// GOOD LUCK 😀
+
+///////////////////////////////////////
+// Optional Chaining
+/*
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
